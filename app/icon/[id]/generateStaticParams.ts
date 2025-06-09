2e325f061c97daa58f86b5ca6f9818406a745e7f
morry-r <anjumade0@gmail.com>
2025-06-09 20:18:07 +0900

    不要なファイル削除

--- app/icon/[id]/generateStaticParams.ts
+++
@@ -1,8 +0,0 @@
-import { getIcons } from "@/lib/api";
-
-export async function generateStaticParams() {
-  const icons = await getIcons();
-  return icons.map((icon) => ({
-    id: icon.id,
-  }));
-} 
